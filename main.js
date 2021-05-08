2dcbab6318a1cc943b444f29afc3877c692dce2e
vinay13000 <74045128+vinay13000@users.noreply.github.com>
2021-05-08 17:50:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,42 +3,42 @@ ctx= canvas.getContext("2d");
 
 
 ctx.beginPath();
-ctx.strokestyle=grey;
+ctx.strokestyle="grey";
 ctx.lineWidth=2;
 ctx.rect(150,143,430,200);
 ctx.stroke();
 
 
 
-    ctx.beginPath();
-ctx.strokestyle=blue;
+ctx.beginPath();
+ctx.strokestyle="blue";
 ctx.lineWidth=2;
 ctx.arc(250,210,40,0,2*Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.strokestyle=black;
+ctx.strokestyle="black";
 ctx.lineWidth=2;
 ctx.arc(350,210,40,0,2*Math.PI);
 ctx.stroke();
 
 
 ctx.beginPath();
-ctx.strokestyle=red;
+ctx.strokestyle="red";
 ctx.lineWidth=2;
 ctx.arc(450,210,40,0,2*Math.PI);
 ctx.stroke();
 
 
 ctx.beginPath();
-ctx.strokestyle=orange;
+ctx.strokestyle="orange";
 ctx.lineWidth=2;
 ctx.arc(300,250,40,0,2*Math.PI);
 ctx.stroke();
 
 
 ctx.beginPath();
-ctx.strokestyle=green;
+ctx.strokestyle="green";
 ctx.lineWidth=2;
 ctx.arc(400,250,40,0,2*Math.PI);
 ctx.stroke();
